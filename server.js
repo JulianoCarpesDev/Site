ff43472e4eb307ab5799e1d8a498bebfc6cebedc
JulianoCarpes <julianocarpes.dev@gmail.com>
2023-01-24 16:50:21 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const rotas = require('./routes')
+
+
+
+app.set('views', path.resolve(__dirname, 'public'));
+app.set('view engine', 'ejs');
+app.use(rotas)
+
+app.listen(3000,()=>{
+    console.log('http://localhost:3000')
+    console.log('servidor conectado')
+})
